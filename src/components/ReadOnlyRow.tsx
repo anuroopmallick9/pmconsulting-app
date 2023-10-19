18e8316339b65a43b0a5ea20f36d25f63380ed6e
anuroopmallick9 <anuroop.mallick@cleverextechnology.com>
2023-10-19 11:15:09 +0530

    ui: assessment page table column width ui change

--- src/components/ReadOnlyRow.tsx
+++ src/components/ReadOnlyRow.tsx
@@ -21,19 +21,11 @@ const ReadOnlyRow = ({
       <td className=" py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3 w-96 text-justify">
         {row.question}
       </td>
-      <td className="w-80 whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-        {row.response}
-      </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-        {row.tools}
-      </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-        {row.document}
-      </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-        {row.rating}
-      </td>
-      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+      <td className="w-80 px-3 py-4 text-sm text-gray-500">{row.response}</td>
+      <td className="w-40 px-3 py-4 text-sm text-gray-500">{row.tools}</td>
+      <td className="w-40 px-3 py-4 text-sm text-gray-500">{row.document}</td>
+      <td className=" px-3 py-4 text-sm text-gray-500">{row.rating}</td>
+      <td className="relative py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
         <a
           href="#"
           className="text-indigo-600 hover:text-indigo-900"
